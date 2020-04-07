555d51398d7a30de1ca50b2c7b3b871b90191deb
Michael Greco <michael.greco07@gmail.com>
2020-04-07 12:01:50 -0400

    opulate UI with saved data

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+populateUi();
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -24,6 +26,24 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localstorage and populate UI
+function populateUi() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
@@ -42,3 +62,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+// initial count and total set
+updateSelectedCount();
